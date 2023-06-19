487545c1edf3a33a269fdd13ebd81d8ac7c5ccdd
Ivan <kovtun65sergey@gmail.com>
2023-06-19 18:45:21 +0300

    add ham menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,71 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        loop: true,
         // margin:10,
-        nav:true,
+        nav: true,
         // autoplay:true,
         // autoplayTimeout:2000,
-        navText: [ '', ' ' ],
+        navText: ['', ' '],
 
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            1100:{
-                items:5
+            1100: {
+                items: 5
             }
         }
     });
-    $( function() {
-        $( "#accordion" ).accordion();
-    } );
+    $(function () {
+        $("#accordion").accordion();
+    });
+
+
+    /* Please ❤ this if you like it! */
+
+
+    (function ($) {
+        "use strict";
+
+
+        //Navigation
+
+        var app = function () {
+            var body = undefined;
+            var menu = undefined;
+            var menuItems = undefined;
+            var init = function init() {
+                body = document.querySelector('body');
+                menu = document.querySelector('.menu-icon');
+                menuItems = document.querySelectorAll('.nav__list-item');
+                applyListeners();
+            };
+            var applyListeners = function applyListeners() {
+                menu.addEventListener('click', function () {
+                    return toggleClass(body, 'nav-active');
+                });
+            };
+            var toggleClass = function toggleClass(element, stringClass) {
+                if (element.classList.contains(stringClass)) element.classList.remove(stringClass); else element.classList.add(stringClass);
+            };
+            init();
+        }();
+
+
+        //Switch light/dark
+
+        $("#switch").on('click', function () {
+            if ($("body").hasClass("light")) {
+                $("body").removeClass("light");
+                $("#switch").removeClass("switched");
+            } else {
+                $("body").addClass("light");
+                $("#switch").addClass("switched");
+            }
+        });
+
+    })(jQuery);
 });
